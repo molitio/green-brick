38313f9153c037cf71b8713cc1e763c317d918bb
Peter Koller <peterkollerlv@live.com>
2022-08-25 12:28:56 +0200

    mui and styled components example (#7)
    * feat(2): added mui & style components
    
    * feat(2): added mui and styled components example

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import Box from "@mui/material/Box";
+
+const StyledLayout = styled.div`
+  background-color: green;
+  height: 100vh;
+`;
 
 const Layout: React.FC<React.PropsWithChildren> = (props) => {
   const { children } = props;
   return (
-    <>
-      hi layout
-      <br />
-      {children}
-    </>
+    <StyledLayout>
+      <Box sx={{ backgroundColor: "yellow", height: "50vh" }}>
+        hi layout
+        <br />
+        {children}
+      </Box>
+    </StyledLayout>
   );
 };
 
