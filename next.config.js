ef937fc1bbfa0f41c4485b36cf51e8e514c7077f
Peter Koller <peterkollerlv@live.com>
2022-10-02 11:10:26 +0200

    chore(31): added fonts

--- next.config.js
+++ next.config.js
@@ -5,6 +5,69 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  crossOrigin: "anonymous",
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=()",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://s3.eu-west-1.amazonaws.com/filestore.molitio.org",
+            /*     process.env.NODE_ENV === "development"
+                ? "*"
+                : "https://s3.eu-west-1.amazonaws.com/filestore.molitio.org", */
+          },
+          {
+            key: "Content-Security-Policy",
+
+            value:
+              process.env.NODE_ENV === "development"
+                ? ``
+                : `
+                    default-src 'self' http://localhost:3000 https://localhost:3000;
+                    script-src 'self' http://localhost:3000 https://localhost:3000;
+                    child-src 'self';
+                    style-src 'self';
+                    font-src 'self' https://s3.eu-west-1.amazonaws.com/filestore.molitio.org;  
+                  `
+                    .replace(/\s{2,}/g, " ")
+                    .trim(),
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     images: {
       remotePatterns: [
