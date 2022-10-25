92b8e93387ad566e47fe0abcce44a6f3663d72b9
Peter Koller <peterkollerlv@live.com>
2022-10-25 10:44:41 -0700

    fix(31): next js config headers fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,49 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Access-Control-Allow-Origin",
+    value:
+      "https://s3.eu-west-1.amazonaws.com https://fonts.googleapis.com https://fonts.gstatic.com",
+  },
+  /*           {
+        key: "Content-Security-Policy",
+        value:
+          process.env.NODE_ENV === "development"
+            ? ""
+            : "default-src 'self' https://s3.eu-west-1.amazonaws.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; child-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com  https://fonts.gstatic.com  https://s3.eu-west-1.amazonaws.com; font-src 'self' https://fonts.googleapis.com  https://fonts.gstatic.com",
+      }, */
+];
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
@@ -9,49 +53,7 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        headers: [
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          },
-
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "geolocation=()",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value:
-              "https://s3.eu-west-1.amazonaws.com https://fonts.googleapis.com https://fonts.gstatic.com",
-          },
-          /*           {
-            key: "Content-Security-Policy",
-            value:
-              process.env.NODE_ENV === "development"
-                ? ""
-                : "default-src 'self' https://s3.eu-west-1.amazonaws.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; child-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com  https://fonts.gstatic.com  https://s3.eu-west-1.amazonaws.com; font-src 'self' https://fonts.googleapis.com  https://fonts.gstatic.com",
-          }, */
-        ],
+        headers: securityHeaders,
       },
     ];
   },
@@ -67,5 +69,3 @@ const nextConfig = {
     },
   },
 };
-
-module.exports = nextConfig;
