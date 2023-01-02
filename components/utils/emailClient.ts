0faac6aba98dcfd61db14335897c6d1d767ba52c
Peter Koller <peterkollerlv@live.com>
2023-01-02 16:41:10 +0100

    chore(65): modification to email client

--- components/utils/emailClient.ts
+++ components/utils/emailClient.ts
@@ -2,8 +2,8 @@ import { SMTPClient } from "emailjs";
 
 export const emailClient = () =>
   new SMTPClient({
-    user: process.env.EMAILJS_USER,
-    password: process.env.EMAILJS_PASSWORD,
-    host: process.env.EMAILJS_HOST,
+    user: process.env.EMAILJS_USER ?? "",
+    password: process.env.EMAILJS_PASSWORD ?? "",
+    host: process.env.EMAILJS_HOST ?? "",
     ssl: true,
   });
