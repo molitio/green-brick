2f4114b35b9a70ccbc4115032a666e390fe9c8c2
Peter Koller <peterkollerlv@live.com>
2023-01-17 12:34:22 +0100

    fix(pilot): type error fix

--- components/NonceContextProvider.tsx
+++ components/NonceContextProvider.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NonceContext } from "../context";
 import { getNonce } from "./utils";
 
-const NonceContextProvider: React.FC<React.PropsWithChildren, Nonce> = (
+const NonceContextProvider: React.FC<React.PropsWithChildren & Nonce> = (
   props
 ) => {
   const { children } = props;
